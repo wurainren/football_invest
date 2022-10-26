9d9023aad1651dfe97bde8bb6def0bf905612c5f
renwu <wurainren@163.com>
2022-10-26 19:15:17 +0800

    初始化项目

---
+++ src/main/java/com/wurainren/football_invest/FootballInvestApplication.java
@@ -0,0 +1,13 @@
+package com.wurainren.football_invest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballInvestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballInvestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wurainren/football_invest/FootballInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wurainren.football_invest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballInvestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
